d4b13f96fc91697a3f9c9f6f295d81bb5ecd92aa
Pozharov Roman <ru.roman.ps@gmail.com>
2017-12-14 17:37:28 +0400

    add gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     minify = require('gulp-csso'),
     rename = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('styles', function() {
   gulp.src('sass/style.scss')
@@ -12,9 +13,9 @@ gulp.task('styles', function() {
   .pipe(sass())
   .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
   .pipe(gulp.dest('css'))
-//  .pipe(minify())
-//  .pipe(rename("style.min.css"))
-//  .pipe(gulp.dest('css'))
+  .pipe(minify())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('css'))
   .pipe(server.reload({stream: true}));
 });
 
@@ -25,6 +26,16 @@ gulp.task('serve', function(){
     },
     notify: false
   });
+});
+
+gulp.task('images', function() {
+  return gulp.src('img/*')
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest('img-min'));
 });
 
 gulp.task ('watch', ['serve', 'styles'], function() {
