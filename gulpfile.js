c925277c4c208667ee314229aa506f646c14b5d4
Pozharov Roman <ru.roman.ps@gmail.com>
2017-12-14 21:24:50 +0400

    tinypng

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     minify = require('gulp-csso'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-tinypng');
+    
 
 gulp.task('styles', function() {
   gulp.src('sass/style.scss')
@@ -28,13 +29,9 @@ gulp.task('serve', function(){
   });
 });
 
-gulp.task('images', function() {
-  return gulp.src('img/*')
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true}),
-    imagemin.svgo()
-  ]))
+gulp.task('tinypng', function() {
+  gulp.src('img/*')
+  .pipe(imagemin('OqvQ0pn0iAaJbR8QeJbvbVf5t41GplSB'))
   .pipe(gulp.dest('img-min'));
 });
 
